11007debf2d0c33ff12800af21d2a257868c54c3
mhammeDev <mhammedvipp@gmail.com>
2025-06-27 19:59:45 +0200

    Student layout

--- front/src/app/etudiant/layout/etudiant-layout.component.ts
+++ front/src/app/etudiant/layout/etudiant-layout.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {NavbarComponent} from '../../shared/components/navbar/navbar.component';
+import {SidebarComponent} from '../../shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-etudiant-layout',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    NavbarComponent,
+    SidebarComponent
   ],
   templateUrl: './etudiant-layout.component.html',
   styleUrl: './etudiant-layout.component.css'
 })
 export class EtudiantLayoutComponent {
 
+  isCollapsed: boolean = true;
+
+  ToggleContainer(): void {
+    console.log('ToggleContainer');
+    this.isCollapsed = !this.isCollapsed;
+  }
+
 }
+
