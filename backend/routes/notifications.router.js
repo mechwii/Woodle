776b179668ef11429f16ce6b37b52831653a7e23
mhammeDev <mhammedvipp@gmail.com>
2025-06-27 18:04:01 +0200

    Adding notification part in the backend

---
+++ backend/routes/notifications.router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const NotificationController = require('../controllers/notifications.controller');
+
+router.post('/add', NotificationController.addNotification);
+/*
+{
+  "code_matiere": "WE4A",
+  "emetteur_id": 2,
+  "type_notification": "publication",
+  "type_destinataire": "groupe",
+  "destinataire_groupe_id": "eleves"
+}
+*/
+
+router.get('/all', NotificationController.getAllNotifications);
+
+router.get('/user/:user_id/:role', NotificationController.getNotificationsForUser);
+/*
+GET /notification/user/3/eleves
+*/
+module.exports = router;
